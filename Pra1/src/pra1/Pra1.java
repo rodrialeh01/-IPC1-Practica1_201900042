5cdf5f559c9af4d5a3983197b0f96a306a576813
Rodrigo Hernández <2823897250101@ingenieria.usac.edu.gt>
2021-08-15 14:52:52 -0600

    Actualización del diseño del tablero
    Se hizo mas grande y mas ordenado el tablero

--- Pra1/src/pra1/Pra1.java
+++ Pra1/src/pra1/Pra1.java
@@ -28,7 +28,7 @@ class programap{
             //
             try{
                 System.out.println("===========     MENÚ PRINCIPAL    ==========");
-                System.out.println("== 1. Inicio                              ==");
+                System.out.println("== 1. Iniciar Juego                       ==");
                 System.out.println("== 2. Reanudar Juego                      ==");
                 System.out.println("== 3. Generar Reportes                    ==");
                 System.out.println("== 4. Salir                               ==");
@@ -61,19 +61,73 @@ class programap{
     }
     
     public void tablero(){
-        for (int i = 8-1; i >=0; i--) {
+        for (int i = 16-1; i >=0; i--) {
             System.out.println("");
-            if (i%2==0) {
-                for (int j = 8-1; j >= 0; j--) {
-                    //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
-                    System.out.print(((j+1)+(8*i)) + " | ");
-                }
-            }else{
+            if (i % 2 == 0) {
                 for (int j = 0; j < 8; j++) {
-                    //System.out.print("[" + i + "," + j + "] = " + (j+1) + " | ");
-                    System.out.print(((j+1)+(8*i)) + " | ");
+                        //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
+                        //System.out.print(((j + 1) + (8 * i)) + " | ");
+                        System.out.print("   |");
                 }
-            }       
+                System.out.println("");
+                System.out.print("--------------------------------");
+            }else{
+                switch(i){
+                    case 1:
+                        for (int j = 8 - 1; j >= 0; j--) {
+                            //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-1))) + "  |");
+                        }
+                        break;
+                    case 3:
+                        for (int j = 0; j < 8; j++) {
+                            //System.out.print("[" + i + "," + j + "] = " + (j+1) + " | ");
+                            if (((j + 1) + (8 * (i-2)))==9) {
+                                System.out.print(((j + 1) + (8 * (i-2))) + "  |");
+                            }else{
+                                System.out.print(((j + 1) + (8 * (i-2))) + " |");
+                            }
+                            
+                        }
+                        break;
+                    case 5:
+                        for (int j = 8 - 1; j >= 0; j--) {
+                            //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-3))) + " |");
+                        }
+                        break;
+                    case 7:
+                        for (int j = 0; j < 8; j++) {
+                            //System.out.print("[" + i + "," + j + "] = " + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-4))) + " |");
+                        }
+                        break;
+                    case 9:
+                        for (int j = 8 - 1; j >= 0; j--) {
+                            //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-5))) + " |");
+                        }
+                        break;
+                    case 11:
+                        for (int j = 0; j < 8; j++) {
+                            //System.out.print("[" + i + "," + j + "] = " + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-6))) + " |");
+                        }
+                        break;
+                    case 13:
+                        for (int j = 8 - 1; j >= 0; j--) {
+                            //System.out.print("[" + i + "," + j + "] = "  + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-7))) + " |");
+                        }
+                        break;
+                    case 15:
+                        for (int j = 0; j < 8; j++) {
+                            //System.out.print("[" + i + "," + j + "] = " + (j+1) + " | ");
+                            System.out.print(((j + 1) + (8 * (i-8))) + " |");
+                        }
+                        break;
+                }
+            }                       
         }
     }
 }
